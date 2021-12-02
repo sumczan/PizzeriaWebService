9ac2a82bc74b78708f45898277e779a43ab16a3e
sumczan <45795394+sumczan@users.noreply.github.com>
2021-12-02 12:01:24 +0100

    Transients and repos

--- PizzeriaWebService/Program.cs
+++ PizzeriaWebService/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PizzeriaWebService.Core.EfModels;
+using PizzeriaWebService.Core.Interfaces.Repositories;
+using PizzeriaWebService.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,25 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<PizzeriaDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+
+builder.Services.AddTransient<PizzeriaDbContext>();
+
+builder.Services.AddTransient<IBeverageRepository, BeverageRepository>();
+builder.Services.AddTransient<ICityRepository, CityRepository>();
+builder.Services.AddTransient<IClientBlacklistRepository, ClientBlacklistRepository>();
+builder.Services.AddTransient<IIngredientRepository, IngredientRepository>();
+builder.Services.AddTransient<IIngredientTypeRepository, IngredientTypeRepository>();
+builder.Services.AddTransient<IOrderAddressRepository, OrderAddressRepository>();
+builder.Services.AddTransient<IOrderBeverageRepository, OrderBeverageRepository>();
+builder.Services.AddTransient<IOrderPizzaIngredientChangeRepository, OrderPizzaIngredientChangeRepository>();
+builder.Services.AddTransient<IOrderPizzaIngredientExtraRepository,OrderPizzaIngredientExtraRepository>();
+builder.Services.AddTransient<IOrderPizzaRepository, OrderPizzaRepository>();
+builder.Services.AddTransient<IOrderPlacedRepository, OrderPlacedRepository>();
+builder.Services.AddTransient<IOrderStatusRepository, OrderStatusRepository>();
+builder.Services.AddTransient<IOrderTypeRepository, OrderTypeRepository>();
+builder.Services.AddTransient<IPizzaIngredientRepository, PizzaIngredientRepository>();
+builder.Services.AddTransient<IPizzaRepository, PizzaRepository>();
+builder.Services.AddTransient<IPizzaSizeRepository, PizzaSizeRepository>();
 
 var app = builder.Build();
 

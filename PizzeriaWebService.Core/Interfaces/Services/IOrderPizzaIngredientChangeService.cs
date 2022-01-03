571b58cee3ce573e771fa6ce4586678aeaf4305f
sumczan <45795394+sumczan@users.noreply.github.com>
2022-01-03 14:46:20 +0100

    Pulled OrderPizzaIngredientChangeService methods to it's parent interface.

--- PizzeriaWebService.Core/Interfaces/Services/IOrderPizzaIngredientChangeService.cs
+++ PizzeriaWebService.Core/Interfaces/Services/IOrderPizzaIngredientChangeService.cs
@@ -1,6 +1,17 @@
-﻿namespace PizzeriaWebService.Core.Interfaces.Services;
+﻿using PizzeriaWebService.Core.DTOs;
+
+namespace PizzeriaWebService.Core.Interfaces.Services;
 
 public interface IOrderPizzaIngredientChangeService
 {
+    Task<IEnumerable<OrderPizzaIngredientChangeDTO>> GetOrderPizzaIngredientChangesAsync();
+    Task<IEnumerable<OrderPizzaIngredientChangeDTO>> GetOrderPizzaIngredientChangesByOrderPizzaIdAsync(int orderPizzaId);
+    Task<OrderPizzaIngredientChangeDTO> GetOrderPizzaIngredientChangeByIdAsync(int id);
+    Task RemoveOrderPizzaIngredientChangeAsync(int id);
 
+    Task<OrderPizzaIngredientChangeDTO> AddOrderPizzaIngredientChangeAsync(
+        OrderPizzaIngredientChangeDTO orderPizzaIngredientChangeDTO);
+
+    Task<OrderPizzaIngredientChangeDTO> UpdateOrderPizzaIngredientChangeAsync(
+        OrderPizzaIngredientChangeDTO orderPizzaIngredientChangeDTO);
 }
